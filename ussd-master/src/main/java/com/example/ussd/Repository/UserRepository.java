507aa8c9f7b87526d76c5b5ad0ef927d0a692e19
KERON JR <keronjunior234@gmail.com>
2023-08-17 15:39:38 +0200

    ok

--- ussd-master/src/main/java/com/example/ussd/Repository/UserRepository.java
+++ ussd-master/src/main/java/com/example/ussd/Repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.ussd.Repository;
 
 //import com.example.ussd.Model.User;
-import com.example.ussd.Model.Userussd;
 import com.example.ussd.Model.Userz;
-import com.example.ussd.Model.UssdRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Userz, Long> {
